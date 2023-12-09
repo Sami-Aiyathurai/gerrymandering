1e7bd9abd898ffc5bc853d2a7cece15f37a77a13
Mia Schildbach <mschildbach@smith.edu>
2023-12-09 14:31:21 -0500

    Code from Friday

--- R/contested_fun.R
+++ R/contested_fun.R
@@ -44,6 +44,76 @@ off_list <- function(election){
   return(positions)
 }
 
+
+
+off_list_df_split <- function(election){
+  pos_types <- unique(election$office)
+  positions <- c()
+  if ("President" %in% pos_types){
+    president <- filter
+  }
+  #This might need to be edited
+  #if ("Senate" %in% pos_types){
+    #positions <- append(x = positions, values = "Senate")
+  #}
+  #if ("Attorney General" %in% pos_types){
+    #positions <- append(x = positions, values = "Attorney General")
+  #}
+  #if ("Secretary of State" %in% pos_types){
+    #positions <- append(x = positions, values = "Secretary of State")
+  #}
+  #return(positions)
+}
+
 off_list(wi_2000)
 off_list(wi_2002)
 
+filter_office <- function(x){
+  x <- x %>%
+    filter(office == "House" | office == "State Senate" | office == "State Assembly" | office == "Senate" | office == "President" | office == "Attorney General" | office == "Secretary of State")
+
+  return(x)
+}
+
+wi_2000 <- filter_office(wi_2000)
+
+df_split <- function(wi_2000) {
+  x <- x %>%
+    filter(office == "House")
+  y <- x %>%
+    filter(office == "General Assembly")
+  pos_x_types <- unique(x$district)
+  pos_y_types <- unique(x$district)
+  positions <- c()
+}
+
+# working to make two dfs this will go inside a function
+wi_2000_house <- wi_2000 %>%
+  filter(office == "House")
+wi_2000_sa <- wi_2000 %>%
+  filter(office == "State Assembly")
+pos_x_types <- unique(wi_2000_house$district)
+pos_ward <- as.list(unique(wi_2000_house$ward))
+pos_y_types <- unique(wi_2000_sa$district)
+wi_2000$new <- ifelse(pos_ward == wi_2000$ward, wi_2000$new, wi_2000)
+wi_2000 %>%
+  filter(ward == pos_ward)
+
+#Wisconsin specific code for separating out state Assembly from hous
+
+wi_sa_2000 <- wi_2000 %>%
+  filter(office == "State Assembly")
+wi_h_2000 <- wi_2000 %>%
+  filter(office == "House")
+
+wi_sa_2000$contested = vector(mode = "logical", length = 9838)
+
+for (district in wi_sa_2000$district) {
+  ifelse(wi_sa_2000$contest_dem[district] == 1 & wi_sa_2000$contest_rep[district] == 1, wi_sa_2000$contested[district] == TRUE, wi_sa_2000$contested[district] == FALSE)
+}
+
+for (ward in wi_sa_2000$ward){
+  for (party in wi_sa_2000$party){
+    ifelse(wi_sa_2000$contest_dem[district] == 1 & wi_sa_2000$contest_rep[district] == 1, wi_sa_2000$contested[district] == TRUE, wi_sa_2000$contested[district] == FALSE)
+  }
+}
