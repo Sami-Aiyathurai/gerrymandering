5cf92bc4a7e34ce9611ebdfac43d5e2235fb0c50
mzelloe <76977819+mzelloe@users.noreply.github.com>
2023-12-18 16:15:36 -0500

    Functions written for finding new vote shares via baseline calculation for each district. MZ standard for control flow in line 175. Now Sami can nest within her loops.

--- R/mz_2.R
+++ R/mz_2.R
@@ -3,9 +3,9 @@ years <- as.character(c("2000", "2002", "2004", "2006", "2008", "2010", "2012", 
 
 everything <- map(.x=years, .f=oe_data_WI)
 
-wi_2010 <- state_data[["wi"]][["2010"]]
-wi_2008 <- state_data[["wi"]][["2008"]]
-wi_2006 <- state_data[["wi"]][["2006"]]
+wi_2010 <- data[["wi"]][["2010"]]
+wi_2008 <- data[["wi"]][["2008"]]
+wi_2006 <- data[["wi"]][["2006"]]
 
 
 # Getting the statewide vote information
@@ -59,6 +59,9 @@ statewide_2006 <- statewide_all[[4]]
 statewide_2008 <- statewide_all[[5]]
 statewide_2010 <- statewide_all[[6]]
 
+statewide_2006 <- statewide_master(wi_2006)
+statewide_2008 <- statewide_master(wi_2008)
+statewide_2010 <- statewide_master(wi_2010)
 
 ### BASELINE
 
@@ -125,77 +128,103 @@ district_func <- function(x, y,...) { # binds together the district based race w
   district_x_year <- candidate_function(district_x_year) # now we're getting candidate vote totals, props for each district
 }
 
+filter_district8 <- function(x) { # this needs to be put in a loop but right now I'm having it set for uncontested district 3 in 2010
+  single_dis <- x %>%
+    filter(district == 8)
+  return(single_dis)
+}
+
+
 joining <- function(x, y,...) { # x is sa_2010, y=statewide_2010
   dis <- filter_district(x) %>% # we're filtering the state assembly data for district in question
-    district_func(x, y)
+    district_func(y)
   return(dis)
 }
+
+joining8 <- function(x, y,...) { # x is sa_2010, y=statewide_2010
+  dis <- filter_district8(x) %>% # we're filtering the state assembly data for district in question
+    district_func(y)
+  return(dis)
+}
+
 
 j1 <- joining(x=sa_2010, y=statewide_2010)
 j2 <- joining(x=sa_2008, y=statewide_2008)
 j3 <- joining(x=sa_2006, y=statewide_2006)
 
+j18 <- joining8(x=sa_2010, y=statewide_2010)
+j28 <- joining8(x=sa_2008, y=statewide_2008)
+j38 <- joining8(x=sa_2006, y=statewide_2006)
+
+
 ## Creating the baseline
 
 # Creating a baseline via trimmed means (dropping highest DEM/REP percents) then averaging
 
-baseline_sa3 <- rbind(j1, j2, j3) # gets all the info in one place
+baseline_function <- function(x, ...) {
+  base_x <- x %>%
+    group_by(office, year, party, district) %>%
+    summarize(total_votes_2p = mean(total_votes_2p),
+              cand_total_votes = mean(cand_total_votes),
+              prop=mean(prop)) %>%
+    pivot_wider(names_from = "party", values_from = prop)
+  base_x2 <- subsetting(base_x)
+  return(base_x2)
+}
 
-base_sa3 <- baseline_sa3 %>% # produces candidate vote totals for the district
-  group_by(office, year, party) %>%
-  summarize(total_votes_2p = mean(total_votes_2p),
-            cand_total_votes = mean(cand_total_votes),
-            prop = mean(prop)) %>%
-  pivot_wider(names_from = "party", values_from = prop) %>%
-  filter(!is.na(DEM)) %>%
-  mutate(REP = 1-DEM) # recodes REP column, seems to get rid of the uncontested district
-# need to test this on different district but is promising
-
-base_sa3_mod <- base_sa3 %>%
-  right_join(base_sa3dem, by = c("DEM", "REP", "office",
-                                 "total_votes_2p", "cand_total_votes", "year")) %>%
-  arrange(REP) # tbh I can't remember why I did this
-
-base_sa3_mod <- base_sa3_mod[-1,] # removes the highest dem percent (senate 2006)
-
-base_sa3_mod <- base_sa3_mod %>%
-  arrange(DEM)
-
-trimmed_sa3 <- base_sa3_mod[-1,] # removing the other highest proportion
-
-trimmed_sa3 <- trimmed_sa3 %>%
-  mutate(district = 3) %>%
-  mutate(diff = DEM - REP) # adding the district column in for groupings, now the maxs are trimmed off
-
-final_trimmed_sa3 <- trimmed_sa3 %>%
-  group_by(district) %>%
-  summarize(avg_total_2p = mean(total_votes_2p),
-            avg_dem_prop = mean(DEM),
-            avg_rep_prop = mean(REP),
-            avg_diff = mean(diff))
-
-est_vt_dem <- final_trimmed_sa3[[2]] * final_trimmed_sa3[[3]]
-est_vt_rep <- final_trimmed_sa3[[2]] * final_trimmed_sa3[[4]]
-
-est_votes <- data.frame(est_votes = c(est_vt_dem, est_vt_rep)) # produces the vote share estimates
-
-# we now need to figure out how to add these votes back into the main DF and we're good
-# then can use the votes to calculate EGs
-
-## SCRATCH CODE DO NOT RUN ##
-
-house_filter <- function(x) { # Ignore this because our focus is on the legislative districts (LD)
-  x <- x %>%
-    filter(office == "House")
+subsetting <- function(x, ...) {
+  tail <- tail(x, n=1)
+  ifelse((is.na(tail$DEM)), x <- dem_prep(x), x <- rep_prep(x)) # standard for control flow MZ
   return(x)
 }
 
-check_wards <- function(x) {
-  uwards <- (unique(x$ward)) # creates uwards as a character vector
-  return(uwards)
+dem_prep <- function(x) {
+  prepped <- x %>%
+    filter(!is.na(DEM)) %>%
+    mutate(REP=1-DEM)
+  return(prepped)
 }
 
-check_districts <- function(x) {
-  ndis <- as.integer(unique(x$district))
-  return(ndis)
+rep_prep <- function(x) {
+  temp <- x %>%
+    filter(!is.na(REP)) %>%
+    mutate(DEM=1-REP)
+  return(temp)
 }
+
+base_sa3 <- baseline_function(baseline_sa3)
+base_sa8 <- baseline_function(baseline_sa8)
+
+slicing_func <- function(x) {
+  hdem <- x %>%
+    arrange(REP)
+  hdem <- hdem[-1,]
+  hrep <- hdem %>%
+    arrange(DEM)
+  trimmed <- hrep[-1,]
+  return(trimmed)
+}
+
+trimmed_sa3 <- slicing_func(base_sa3)
+trimmed_sa8 <- slicing_func(base_sa8)
+
+trimmed_func <- function(x) {
+  x <- x %>%
+    mutate(district = i) %>% # where i is whatever district it is on
+    mutate(diff = DEM - REP) %>%
+    group_by(district) %>%
+    summarize(avg_total_2p = mean(total_votes_2p),
+              avg_dem_prop = mean(DEM),
+              avg_rep_prop = mean(REP),
+              avg_diff = mean(diff))
+  return(x)
+}
+
+trimmed_sa3 <- trimmed_func(trimmed_sa3)
+
+vote_estimate <- function(x) {
+  est_dem <- x[[2]] * x[[3]]
+  est_rep <- x[[2]] * x[[4]]
+  return(est_dem, est_rep)
+}
+
