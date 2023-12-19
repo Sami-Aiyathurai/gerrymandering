8e14ceb0a81ac065fd6c4c78572992913c8665a9
Sami Aiyathurai <53624695+Sami-Aiyathurai@users.noreply.github.com>
2023-12-18 19:05:49 -0500

    so much stuff alot of moving tiny functions around to make more intuitive to run and then grouping into files/bigger functions

--- R/molly_file.R
+++ R/molly_file.R
@@ -1,78 +1,35 @@
-un_2010 <- wi_full_sa_di[["2010"]][["uncontested"]]
-
+wi_full_sa_di <- sa_contest_all_wi()
 SAsa_2010 <- wi_full_sa_di[["2010"]]
+SAsa_2010_list <- split(SAsa_2010, SAsa_2010$contested)
+uncon_wi_2010 <- SAsa_2010_list[["uncontested"]]
+#gets uncontested data for year
 
-
-
-# Use functionality from mz_2 to estimate baselines for all uncontested districts in 2010
 
 wi_2010 <- access_state_year("wi", "2010", data)
 wi_2008 <- access_state_year("wi", "2008", data)
 wi_2006 <- access_state_year("wi", "2006", data)
+#gets statewide data for year and two previous years
 
+#actually running on statewide master
 statewide_2010 <- statewide_master(wi_2010)
 statewide_2008 <- statewide_master(wi_2008)
 statewide_2006 <- statewide_master(wi_2006)
 
-SAsa_2010_list <- split(SAsa_2010, SAsa_2010$contested)
-uncon_wi_2010 <- SAsa_2010_list[["uncontested"]]
-
-#uncon_wi_2010 <- uncon_wi_2010 %>%
- # select(ward, district, office, total.votes, party, candidate, votes, year, contested)
-
-
-
 un_districts_2010 <- check_districts(uncon_wi_2010)
-# refresh
+# empty list to store data for year
 districts <- list()
-# this is the loop that works and for every uncontested
-# district in 2010 that pulls from uncon_wi_2010 the dis num
-# saves as character, then appends that way
+test <- list()
+# gets data for uncontested districts for year
 for (i in un_districts_2010) {
-  print(i)
-  print("i in undistricts")
   temp <- uncon_wi_2010 %>%
     filter(district == i) %>%
     subset(select=-c(contest_r, contest_d, contested))
   dis_name <- as.character(i)
-  districts[[dis_name]] <- district_func(temp, statewide_2010)
+  year1 <- district_func(temp, statewide_2010)
+  year2 <- district_func(temp, statewide_2008)
+  year3 <- district_func(temp, statewide_2006)
+  districts[[dis_name]] <- rbind(year1, year2, year3)
+  test[[dis_name]][["2010"]] <- year1
+  test[[dis_name]][["2008"]] <- year2
+  test[[dis_name]][["2006"]] <- year3
   }
-
-temp <- uncon_wi_2010 %>%
-  filter(district == 3) %>%
-  subset(select=-c(contest_r, contest_d, contested))
-testing_nw <- district_func(temp, statewide_2010)
-testing_w <- district_func(sa_3_2010, statewide_2010)
-
-
-
-
-
-
-
-
-
-
-filter_district <- function(x,i) { # this needs to be put in a loop but right now I'm having it set for uncontested district 3 in 2010
-  single_dis <- x %>%
-    filter(district == i)
-}
-
-joining_ <- function(){
-  dist_func_year <- list()
-  for(i in seq(2000, 2020, 2)){
-    year <- toString(i)
-    dfs_wi[[year]] <- oe_data_WI(year)
-    dfs_mi[[year]] <- oe_data_MI(year)
-  }
-  dfs <- list()
-  dfs[["wi"]] <- dfs_wi
-  dfs[["mi"]] <- dfs_mi
-  return(dfs)
-}
-
-
-
-x <- district_func(x=sa_2010, y=statewide_2010)
-
-x1 <- joining(sa_2010, statewide_2010)
